52cc8bc9cc11b95f7d6e4ae4d91bb4ac2251e4f3
Alex Bertotto <ajb5206@gmail.com>
2024-02-16 08:53:53 -0800

    add get response for an api and dummy data

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express() // app holds express
 const PORT = 8000
 
+const savage = {
+	'age': 29,
+	'birthName': 'Something something SOmething',
+	'birthLocation': 'London, England'
+}
 
 app.get('/', (request, response) =>{
 	response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (request, response) =>{
+	response.json(savage)
 })
 
 app.listen(PORT, () => {
