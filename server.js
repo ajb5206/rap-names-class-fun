b71cdc0267adb50cc94a8345b60e949914e85f33
Alex Bertotto <ajb5206@gmail.com>
2024-02-19 11:34:22 -0800

    changed object properties to lowercase and added method to api to handle case insensitivity

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const app = express() // app holds express
 const PORT = 8000
 
 const rappers = {
-	'21 Savage':{
+	'21 savage':{
 			'age': 29,
 			'birthName': 'Something something SOmething',
 			'birthLocation': 'London, England'
 	},
-	'Chance the Rapper':{
+	'chance the rapper':{
 		'age': 29,
 		'birthName': 'Chancelor Bennett',
 		'birthLocation': 'Chicago, Illinois'
 	},
-	'Dylan':{
+	'dylan':{
 		'age': 29,
 		'birthName': 'Dylan',
 		'birthLocation': 'Dylan'
@@ -25,13 +25,12 @@ app.get('/', (request, response) =>{
 })
 
 app.get('/api/:rapperName', (request, response) =>{
-	const rappersName = request.params.rapperName
+	const rappersName = request.params.rapperName.toLowerCase()
 	if(rappers[rappersName]){
 		response.json(rappers[rappersName])
 	}else{
 		response.json(rappers['Dylan'])
 	}
-	//response.json(rappers)
 })
 
 app.listen(PORT, () => {
